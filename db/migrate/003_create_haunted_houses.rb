bdb302a5ed95e649f57901bc66dd0a6510cd10bd
Steve Womble <swomble@aol.com>
2020-06-27 19:32:03 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,8 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |t|
-      
+    t.string :name 
+    t.string :location
+    t.
   end
 end
