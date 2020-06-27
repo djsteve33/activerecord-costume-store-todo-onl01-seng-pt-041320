f084c15132449588c728a94eb9f2f399cc07001c
Steve Womble <swomble@aol.com>
2020-06-27 19:25:07 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,6 +3,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
       t.string :name 
-      t.string
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employee_count
+      t.boolean 
   end
 end
